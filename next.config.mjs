f42b40349ba3fb2f5a03a9621d892a1471c39708
cabyz-admin <admin@cabyz.com>
2025-06-04 16:24:48 -0600

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  transpilePackages: [
+    '@v1/ui',
+    '@v1/supabase',
+    '@v1/analytics',
+    '@v1/kv',
+    '@v1/logger'
+  ]
 };
 
 export default nextConfig;
